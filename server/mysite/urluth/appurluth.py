6e384ab4aecf9146343b3625e8e33040a812d8a5
Réchèr <recher_burp@yahoo.fr>
2016-12-13 01:06:19 +0100

    [dev] Utilisation correcte de jinja pour générer via un template la page de redirection.

--- server/mysite/urluth/appurluth.py
+++ server/mysite/urluth/appurluth.py
@@ -8,69 +8,31 @@ from .build_dict_urls import final_urls_from_key_urls
 # http://flask.pocoo.org/docs/0.11/blueprints/
 app_urluth = Blueprint("app_urluth", __name__, template_folder='templates')
 
-# RECTODO : on aura bientôt plus besoin de ça.
-#TEMPLATE_FILE_PATH = "urluth" + os.sep + "template.html"
-TEMPLATE_FILE_PATH = "/home/Recher/mysite/urluth/template.html"
-# RECTODO : debug. Serveur local.
-TEMPLATE_FILE_PATH = 'C:\\Recher\\git\\urluth\\server\\mysite\\urluth\\template.html'
-
-FINAL_URL_TEXT_ID = "{{final_url}}"
-
-
-def init_templates_html():
-
-    template_html = "failed reading template html"
-    with open(TEMPLATE_FILE_PATH, encoding="utf-8") as template_file:
-        template_html = template_file.read()
-
-    PAGE_STATIC_TEXTS = (
-        (
-            "found", "{{here_is_link_fr}}",
-            "Voici le lien qui vous intéresse :"),
-        (
-            "found", "{{here_is_link_en}}",
-            "Here is the desired link :"),
-        (
-            "not_found", "{{here_is_link_fr}}",
-            "Désolé, le lien que vous voulez est introuvable. En voici un autre :"),
-        (
-            "not_found", "{{here_is_link_en}}",
-            "Sorry, your link can not be found. Here is another one :"),
-        (
-            "not_found", FINAL_URL_TEXT_ID,
-            "http://recher.wordpress.com"),
-    )
-
-    tplh_key_found = template_html
-    tplh_key_not_found = template_html
-
-    for (is_found, text_id, text) in PAGE_STATIC_TEXTS:
-        if is_found == "found":
-            tplh_key_found = tplh_key_found.replace(text_id, text)
-        else:
-            tplh_key_not_found = tplh_key_not_found.replace(text_id, text)
-
-    return tplh_key_found, tplh_key_not_found
-
-tplh_key_found, tplh_key_not_found = init_templates_html()
-
 
 # Si on a besoin de router n'importe quelle adresse et de l'analyser après :
 # http://flask.pocoo.org/snippets/57/
 # Mais là c'est pas ça que je veux faire.
 
+# RECTODO : changer les guillemets double en simple, partout où il faut.
+
 @app_urluth.route('/',  methods=["GET"])
 def urluthGet():
 
-    # RECTODO : ça, ça marche. Maintenant faut mettre un vrai template.
-    return render_template('rectodo.html')
-
     key_url = request.args.get("u", "")
     if not key_url.isalnum():
-        return tplh_key_not_found
-
-    final_url = final_urls_from_key_urls.get(key_url)
-    if final_url is None:
-        return tplh_key_not_found
+        final_url = None
     else:
-        return tplh_key_found.replace(FINAL_URL_TEXT_ID, final_url)
+         final_url = final_urls_from_key_urls.get(key_url)
+
+    if final_url is None:
+        params_template = {
+            'here_is_link_fr': "Désolé, le lien que vous voulez est introuvable. En voici un autre :",
+            'here_is_link_en': "Sorry, your link can not be found. Here is another one :",
+            'final_url': 'http://recher.wordpress.com', }
+    else:
+        params_template = {
+            'here_is_link_fr': "Voici le lien qui vous intéresse :",
+            'here_is_link_en': "Here is the desired link :",
+            'final_url': final_url, }
+
+    return render_template('template.html', **params_template)
